5c8fc0b5f3f7d2d26f7932d290c5ea39015a1c0b
Gabriel Brandão <gabriel.silva.brandao7@gmail.com>
2019-08-24 10:43:44 -0300

    Create Android Studio Project and update dependencies

---
+++ app/src/main/java/brandao/gabriel/mostradecursos2019/TelaInicial.kt
@@ -0,0 +1,12 @@
+package brandao.gabriel.mostradecursos2019
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaInicial : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_inicial)
+    }
+}
