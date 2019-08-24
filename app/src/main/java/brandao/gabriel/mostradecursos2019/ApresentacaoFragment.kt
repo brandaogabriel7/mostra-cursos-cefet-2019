12778aaee8fc2da636a527a0182b0236f6b1eb07
Gabriel Brandão <gabriel.silva.brandao7@gmail.com>
2019-08-24 18:36:49 -0300

    Create fragments for the main tabs

---
+++ app/src/main/java/brandao/gabriel/mostradecursos2019/ApresentacaoFragment.kt
@@ -0,0 +1,13 @@
+package brandao.gabriel.mostradecursos2019
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ApresentacaoFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_apresentacao, container, false)
+    }
+}
