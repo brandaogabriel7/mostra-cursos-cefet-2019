97e480cb992d09c2680916f9559e78b1bfb6dc4e
Gabriel Brandão <gabriel.silva.brandao7@gmail.com>
2019-08-24 18:37:08 -0300

    Finish navigation bottom view

--- app/src/main/java/brandao/gabriel/mostradecursos2019/TelaInicial.kt
+++ app/src/main/java/brandao/gabriel/mostradecursos2019/TelaInicial.kt
@@ -2,11 +2,35 @@ package brandao.gabriel.mostradecursos2019
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 
 class TelaInicial : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_inicial)
+
+        val bottomNav:BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, ApresentacaoFragment()).commit()
+
+        bottomNav.setOnNavigationItemSelectedListener {
+            var selectedFragment:Fragment
+            when (it.itemId) {
+                R.id.nav_apresentacao -> {
+                    selectedFragment = ApresentacaoFragment()
+                }
+                R.id.nav_programacao -> {
+                    selectedFragment = ProgramacaoFragment()
+                }
+                R.id.nav_cursos -> {
+                    selectedFragment = CursosFragment()
+                }
+                else -> selectedFragment = ApresentacaoFragment()
+            }
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, selectedFragment).commit()
+            true
+        }
     }
 }
