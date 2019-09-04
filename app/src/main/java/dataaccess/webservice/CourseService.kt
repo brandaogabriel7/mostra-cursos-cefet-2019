8c484240a2b76b9a5f3639cf63f3ca9a9f793784
Gabriel Brandão <gabriel.silva.brandao7@gmail.com>
2019-09-04 17:45:29 -0300

    Change API interface

--- app/src/main/java/dataaccess/webservice/CourseService.kt
+++ app/src/main/java/dataaccess/webservice/CourseService.kt
@@ -2,10 +2,26 @@ package dataaccess.webservice
 
 import brandao.gabriel.mostradecursos2019.entity.Course
 import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
+const val BASE_URL = "https://mostra-cursos-api.herokuapp.com"
+
 interface CourseService {
+
     @GET("courses")
     fun getCourseList() : Call<ArrayList<Course>>
+
+    companion object {
+
+        operator fun invoke() : CourseService {
+            return Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+                    .create(CourseService::class.java)
+        }
+    }
 }
 
