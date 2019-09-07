f737f5eecf7ba0448c737e63d9f1c1bc2006c8d3
Gabriel Brandão <gabriel.silva.brandao7@gmail.com>
2019-09-07 11:37:18 -0300

    Migrate project to Android x

--- app/src/main/java/brandao/gabriel/mostradecursos2019/ui/ProgramacaoFragment.kt
+++ app/src/main/java/brandao/gabriel/mostradecursos2019/ui/ProgramacaoFragment.kt
@@ -1,13 +1,13 @@
 package brandao.gabriel.mostradecursos2019.ui
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import brandao.gabriel.mostradecursos2019.R
 
-class ProgramacaoFragment : Fragment() {
+class ProgramacaoFragment : androidx.fragment.app.Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_programacao, container, false)
     }
--- app/src/main/java/brandao/gabriel/mostradecursos2019/ui/TelaInicial.kt
+++ app/src/main/java/brandao/gabriel/mostradecursos2019/ui/TelaInicial.kt
@@ -1,9 +1,9 @@
 package brandao.gabriel.mostradecursos2019.ui
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
-import android.support.v4.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.fragment.app.Fragment
 import android.widget.Toast
 import brandao.gabriel.mostradecursos2019.R
 import brandao.gabriel.mostradecursos2019.entity.Course
@@ -48,13 +48,13 @@ class TelaInicial : AppCompatActivity() {
 
     fun setupBottomNavigationView() {
 
-        val bottomNav:BottomNavigationView = findViewById(R.id.bottom_navigation)
+        val bottomNav: BottomNavigationView = findViewById(R.id.bottom_navigation)
 
         supportFragmentManager.beginTransaction().replace(R.id.fragment_container, ApresentacaoFragment()).commit()
 
         bottomNav.setOnNavigationItemSelectedListener {
 
-            var selectedFragment:Fragment
+            var selectedFragment: androidx.fragment.app.Fragment
 
             when (it.itemId) {
                 R.id.nav_apresentacao -> {
